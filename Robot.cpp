ae7c5ee3cb727d9398df9aba792cdd4c0b31d4cb
cmyhj <2484684043@qq.com>
2024-02-02 15:19:01 +0800

    second_try

--- Robot.cpp
+++ Robot.cpp
@@ -6,35 +6,40 @@
 using namespace std;
 
 Robot::Robot(int time, int team, int number, int type) {
-    LastCommandTime=time;
+    LastCommandTime = time;
     Team = team;
     Number = number;
     Type = type;
 }
 
 void Robot::HotDamageAndSet(int time) {
-        if (!isDead ) {//找到对应机器人
+    if (!isDead) {//找到对应机器人
+        int TimeGap = time - LastCommandTime;
+        if (Hot > Hot_Max) {//过热
             int HotGap = Hot - Hot_Max;
-            int TimeGap = time - LastCommandTime;
-            if (HotGap > TimeGap) {//热量未减完
+
+            if (HotGap > TimeGap) {//热量未减至小于Max
                 HP -= (HotGap * 2 - TimeGap + 1) * TimeGap / 2;
-                Hot -= TimeGap;
-            } else {//热量减完
+            } else {//热量减到小于Max
                 HP -= HotGap * (HotGap + 1) / 2;
-                Hot = 0;
             }
         }
+        Hot -= TimeGap;//set
+        if (Hot<0){
+            Hot=0;//热量不小于0
+        }
     }
+}
 
 void Robot::TimeSet(int time) {
-    LastCommandTime=time;
+    LastCommandTime = time;
 }
 
 void Robot::TellIsDead() {
     if (HP <= 0) {//似了
         isDead = true;
-        std::cout<<"D "<<Team<<" "<<Number<<std::endl;
-        std::cout<<"HP:"<<HP<<std::endl;//<<""<<Number<<std::endl;
+        std::cout << "D " << Team << " " << Number << std::endl;
+        std::cout << "TIME:" << LastCommandTime << std::endl;//<<""<<Number<<std::endl;
     }
 }
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/CMYHJ/Desktop/寒假任务/shijue_homework
+# Install script for directory: C:/Users/CMYHJ/Desktop/hanjiarenwu/shijue_homework
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/CMYHJ/Desktop/寒假任务/shijue_homework/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/CMYHJ/Desktop/hanjiarenwu/shijue_homework/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
